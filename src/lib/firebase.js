41f713f5bcbcd98fac8048442cfe076e522537d9
Ashish-Ranjan007 <ashishranjan1729@outlook.com>
2022-02-16 15:45:48 +0530

    second commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { FieldValue, getFirestore } from 'firebase/firestore';
+
 import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-	apiKey: 'AIzaSyBvpOjl_quKIu-4txr7lbsL1-JIjSvhZjY',
-	authDomain: 'react-instagram-clone-5e550.firebaseapp.com',
-	projectId: 'react-instagram-clone-5e550',
-	storageBucket: 'react-instagram-clone-5e550.appspot.com',
-	messagingSenderId: '260211671946',
-	appId: '1:260211671946:web:ec291d4dd5c00fe84bbe73',
+	// Paste your config here
 };
 
 const firebase = initializeApp(config);
