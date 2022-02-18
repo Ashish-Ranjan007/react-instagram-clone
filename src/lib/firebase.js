b96180c8eaf7d0797297c2852a473a0ccb5d11ae
Ashish-Ranjan007 <ashishranjan1729@outlook.com>
2022-02-18 12:28:47 +0530

    test commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,6 +6,12 @@ import 'firebase/auth';
 
 const config = {
 	// Paste your config here
+	apiKey: 'AIzaSyBvpOjl_quKIu-4txr7lbsL1-JIjSvhZjY',
+	authDomain: 'react-instagram-clone-5e550.firebaseapp.com',
+	projectId: 'react-instagram-clone-5e550',
+	storageBucket: 'react-instagram-clone-5e550.appspot.com',
+	messagingSenderId: '260211671946',
+	appId: '1:260211671946:web:ec291d4dd5c00fe84bbe73',
 };
 
 const firebase = initializeApp(config);
